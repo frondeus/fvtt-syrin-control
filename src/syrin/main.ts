7679c9612ea52c622883e448620d2eadb2e007ad
Wojciech Polak <frondeus@gmail.com>
2023-01-20 08:51:07 +0100

    Move socket code to its module

--- src/syrin/main.ts
+++ src/syrin/main.ts
@@ -5,43 +5,13 @@ import { onPlaylistTab } from './ui/playlist';
 import { openElements } from './ui/elements';
 
 import { MODULE } from './services/utils';
-import { Context, resolve } from './services/context';
+import { resolve } from './services/context';
 import { FVTTGameImpl } from './services/game';
 import { RawApiImpl } from './services/raw';
 import { createProxies } from './sounds';
-import { SocketCalls } from './socket';
-// import { openDebug } from './ui/debug';
+import { setupSocket } from './socket';
 
 
-function setupSocket(ctx: Context): Promise<SocketlibSocket> {
-	return new Promise((resolve) => {
-		const interval = setInterval(() => {
-			let socket = ctx.game.socket;
-			if(socket !== undefined) {
-				if (ctx.game.isGM()) {
-					ctx.game.socket?.register(SocketCalls.PlayAmbient, (id, sound) => {
-						ctx.syrin.playAmbientSound(id, sound);
-					});
-					ctx.game.socket?.register(SocketCalls.StopAmbient, (id, userId) => {
-						ctx.syrin.stopAmbientSound(id, userId);
-					});
-					ctx.game.socket?.register(SocketCalls.PlayerJoined, (name) => {
-						return ctx.api.playerJoined(name);
-					});
-					ctx.utils.info("SocketLib registered | GM");
-				}
-				else {
-					ctx.game.socket?.register(SocketCalls.PlayAmbient, (_id, _sound) => { });
-					ctx.game.socket?.register(SocketCalls.StopAmbient, (_id, _userId) => { });
-					ctx.game.socket?.register(SocketCalls.PlayerJoined, () => {});
-					ctx.utils.info("SocketLib registered | Player");
-				}
-				clearInterval(interval);
-				resolve(socket);
-			}
-		}, 200); 
-	});
-}
 
 Hooks.once('init', function () {
 	console.log('SyrinControl | Initializing...');
--- src/syrin/socket.ts
+++ src/syrin/socket.ts
@@ -1,3 +1,4 @@
+import { Context } from "./services/context";
 import { MODULE } from "./services/utils";
 
 export enum SocketCalls {
@@ -11,3 +12,33 @@ export let socket: SocketlibSocket | undefined = undefined;
 Hooks.once('socketlib.ready', () => {
 	socket = socketlib.registerModule(MODULE);
 });
+
+export function setupSocket(ctx: Context): Promise<SocketlibSocket> {
+	return new Promise((resolve) => {
+		const interval = setInterval(() => {
+			let socket = ctx.game.socket;
+			if(socket !== undefined) {
+				if (ctx.game.isGM()) {
+					ctx.game.socket?.register(SocketCalls.PlayAmbient, (id, sound) => {
+						ctx.syrin.playAmbientSound(id, sound);
+					});
+					ctx.game.socket?.register(SocketCalls.StopAmbient, (id, userId) => {
+						ctx.syrin.stopAmbientSound(id, userId);
+					});
+					ctx.game.socket?.register(SocketCalls.PlayerJoined, (name) => {
+						return ctx.api.playerJoined(name);
+					});
+					ctx.utils.info("SocketLib registered | GM");
+				}
+				else {
+					ctx.game.socket?.register(SocketCalls.PlayAmbient, (_id, _sound) => { });
+					ctx.game.socket?.register(SocketCalls.StopAmbient, (_id, _userId) => { });
+					ctx.game.socket?.register(SocketCalls.PlayerJoined, () => {});
+					ctx.utils.info("SocketLib registered | Player");
+				}
+				clearInterval(interval);
+				resolve(socket);
+			}
+		}, 200); 
+	});
+}
