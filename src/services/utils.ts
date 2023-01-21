185bf2a9e6b2b2feb8653f9df9a39abb57a7c8d9
Wojciech Polak <frondeus@gmail.com>
2023-01-21 11:38:12 +0100

    Tracing

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -29,8 +29,7 @@ export class Utils {
 	trace(...args: any[]) {
 		if (this.traceEnabled()) {
 			const first = 'SyrinControl | ' + args.shift();
-			// console.trace(first, ...args);
-			console.info(first, ...args);
+			console.trace(first, ...args);
 		}
 	}
 
