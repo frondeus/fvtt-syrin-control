70f67e034ea7a44d5189a304cd532b7116e3343e
Wojciech Polak <frondeus@gmail.com>
2023-03-26 10:55:09 +0200

    Fix jest

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import { compilerOptions } from './tsconfig.json';
+
+import type { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+const config: Config = {
+	transform: {
+    '^.+\\.svelte$': [
+        'svelte-jester',
+        {
+            preprocess: true
+        }
+    ],
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest'
+	},
+	moduleFileExtensions: ['js', 'ts', 'svelte'],
+	testEnvironment: 'jsdom',
+	modulePathIgnorePatterns: ['<rootDir>/dypress'],
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+		prefix: '<rootDir>' + compilerOptions.baseUrl
+	})
+};
+
+export default config;
