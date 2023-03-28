24b4c66c0dc65b9eb22c0d190ccbfe13a86c9bbb
Wojciech Polak <frondeus@gmail.com>
2023-03-28 21:58:33 +0200

    Actually fix migrating

--- src/migrations.ts
+++ src/migrations.ts
@@ -9,14 +9,34 @@ function migrateAmbientSoundPaths(ctx: Context) {
 			const splitted = path.split(':');
 			const ty = splitted[1];
 			const id = Number(splitted[2].split('.')[0]);
-			(ambientSound as any).flags.syrinscape = {
-				type: ty,
-				[ty]: id
-			};
-			(ambientSound as any).path = 'syrinscape.wav';
+			ambientSound.update({
+				flags: {
+					syrinscape: {
+						type: ty,
+						[ty]: id
+					}
+				},
+				path: 'syrinscape.wav'
+			});
 			ctx.utils.trace('Migrated', { ambientSound });
 		}
 	}
 }
 
-export const migrations = [migrateAmbientSoundPaths];
+function migratePlaylistSoundPaths(ctx: Context) {
+	const playlistSounds = (ctx.game.getPlaylists()?.contents ?? []).flatMap(
+		(playlist) => playlist.sounds.contents
+	);
+	for (const playlistSound of playlistSounds) {
+		const path = (playlistSound as any).path as string;
+		if (path.startsWith('syrinscape:')) {
+			ctx.game.notifyInfo('migration.playlistSound.path', { name: playlistSound.name });
+			playlistSound.update({
+				path: 'syrinscape.wav'
+			});
+			ctx.utils.trace('Migrated', { playlistSound });
+		}
+	}
+}
+
+export const migrations = [migrateAmbientSoundPaths, migratePlaylistSoundPaths];
